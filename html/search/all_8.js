c36710e5192f7a8b20f196afb6006c2aefa56f95
jairoM26 <jairomendezmartinez@gmail.com>
2017-06-02 13:56:04 -0600

    Final

--- html/search/all_8.js
+++ html/search/all_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['liebmann',['Liebmann',['../classmethods_1_1Liebmann.html',1,'methods']]],
-  ['liebmann',['Liebmann',['../classmethods_1_1Liebmann.html#a7a095aca7a7556f4eec22c2c3528f436',1,'methods::Liebmann::Liebmann()'],['../classmethods_1_1Liebmann.html#a422c224247ccf4cb7e997553436aa59e',1,'methods::Liebmann::liebmann(short int n, short int m, T bottom, T top, T left, T right, std::vector&lt; T &gt; &amp;solution)']]]
+  ['liebmann',['Liebmann',['../classmethods_1_1Liebmann.html#a7a095aca7a7556f4eec22c2c3528f436',1,'methods::Liebmann::Liebmann()'],['../classmethods_1_1Liebmann.html#a883dbcefab3121f882abefd7740fe057',1,'methods::Liebmann::liebmann(short int n, short int m, T bottom, T top, T left, T right, std::vector&lt; T &gt; &amp;solution, T umbral)']]]
 ];
--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['fill',['fill',['../classanpi_1_1Matrix.html#ad7b7f6f61d23ae8ccd7e3a19e82a8c63',1,'anpi::Matrix::fill(const T val)'],['../classanpi_1_1Matrix.html#a2df6d413691d4aa8d0e958eb74ee394b',1,'anpi::Matrix::fill(const T *mem)']]],
-  ['fix_5fx',['fix_x',['../namespaceHeatMap.html#a6b883793552fc25a30f40c323d68eb1f',1,'HeatMap']]]
+  ['fix_5fx',['fix_x',['../namespaceHeatMap.html#aab85b8e2aacfbd855e550a1c3279bdf0',1,'HeatMap']]],
+  ['fix_5fy',['fix_y',['../namespaceHeatMap.html#a0d3d1d62800a09ba8f98a926bfcf4101',1,'HeatMap']]]
 ];
